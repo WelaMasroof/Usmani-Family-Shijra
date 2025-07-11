cd53848b756a5a6f003d4b3b986f7151401fa5ff
Umar Farooq <uummeerr0786@gmail.com>
2025-07-11 15:03:13 +0500

    Delete Admin App directory

--- Admin App/lib/graph pages/graphql_config.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-
-ValueNotifier<GraphQLClient> graphqlClient = ValueNotifier(
-  GraphQLClient(
-    link: HttpLink("https://your-vercel-url.vercel.app/graphql"),
-    cache: GraphQLCache(),
-  ),
-);
