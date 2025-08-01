4fbd02316abc26c8bf3384f3ae4cd0e4eb0800c4
WelaMasroof <m.f.h.usmani02@gmail.com>
2025-08-01 12:36:05 +0500

    changed pass

--- FastAPI/db.py
+++ FastAPI/db.py
@@ -2,7 +2,7 @@ from neo4j import GraphDatabase
 
 driver = GraphDatabase.driver(
     uri="neo4j+s://3ac3a9cd.databases.neo4j.io",
-    auth=("neo4j", "DorBjWkdqqulFCgJ6is37YLxWmXmd9_OAHXN3N-KfJ0")
+    auth=("neo4j", "")
 )
 
 
